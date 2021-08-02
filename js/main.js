ea890b6f7fbf9566882e46a9f161c17564fd39e5
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2021-08-02 23:48:20 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -136,5 +136,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " actives";
-  setTimeout(showSlides, 500);
+  setTimeout(showSlides, 5000);
 }
